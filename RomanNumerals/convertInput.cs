40af6ff66eaa3564bb5b3599a43e8c311728a03d
Andy Million <amillion3@gmail.com>
2018-09-25 21:16:31 -0500

    initial setup complete

---
+++ RomanNumerals/convertInput.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RomanNumerals
+{
+    public class ConvertInput
+    {
+        public string ConvertNumberToRomanNumeral(int inputNumber) 
+        {
+            if (inputNumber < 10)
+            {
+
+            }
+            return "I";
+        }
+    }
+}
